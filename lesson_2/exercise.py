a8f3d172b537063f8a70d32fc6eaf3d746e3fc4f
Aleksandr Bronza <aleksandr.bronza@scoutbee.com>
2020-06-22 21:38:05 +0300

    lesson 2

--- lesson_2/exercise.py
+++ lesson_2/exercise.py
@@ -18,7 +18,7 @@ class Vehicle:
         print('Beeeeeeeeep')
 
     def travel_time(self, distance):
-        return distance/self.max_speed
+        return distance / self.max_speed
 
 
 class Car(Vehicle):
@@ -27,7 +27,7 @@ class Car(Vehicle):
 
     def travel_time(self, distance):
         turbo_boost = 2
-        time = distance/(self.max_speed*turbo_boost) * 60
+        time = distance / (self.max_speed * turbo_boost) * 60
         return time
 
 
@@ -35,7 +35,7 @@ class Truck(Car):
     weight = 10
 
     def travel_time(self, distance):
-        time = distance/(self.max_speed/self.weight) * 60
+        time = distance / (self.max_speed / self.weight) * 60
         return time
 
 
@@ -44,6 +44,7 @@ big_truck = Truck('Volvo', 'White', 6, 100)
 print(big_truck.travel_time(100), 'in minutes')
 big_truck.beeping()
 print(sport_car.travel_time(100), 'in minutes')
+
 
 # 2) Создать класс магазина. Конструктор должен инициализировать
 # значения: «Название магазина» и «Количество проданных
@@ -78,3 +79,60 @@ print(small_shop.sold_quantity, '== 5')
 print(small_shop.show_all_sold_products(), 'all products 25+5')
 print(small_shop.increase_products(5))
 print(Shop.sold_products)
+
+
+# Создать класс точки, реализовать конструктор который
+# инициализирует 3 координаты (Class): Определенный программистом
+# тип данных.x, y, z). Реалзиовать методы для
+# получения и изменения каждой из координат. Перегрузить для этого
+# класса методы сложения, вычитания, деления умножения.
+# Перегрузить один любой унарный метод.
+# Ожидаемый результат: умножаю точку с координатами 1,2,3 на
+# другую точку с такими же координатами, получаю результат 1, 4, 9.
+
+
+class Point:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __str__(self):
+        return f'x:{self.x}, y:{self.y}, z:{self.z}'
+
+    def __pos__(self):
+        x = self.x + 1
+        y = self.y + 1
+        z = self.z + 1
+        return Point(x, y, z)
+
+    def __mul__(self, other):
+        x = self.x * other.x
+        y = self.y * other.y
+        z = self.z * other.z
+        return Point(x, y, z)
+
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        z = self.z + other.z
+        return Point(x, y, z)
+
+    def __sub__(self, other):
+        x = self.x - other.x
+        y = self.y - other.y
+        z = self.z - other.z
+        return Point(x, y, z)
+
+    def __truediv__(self, other):
+        x = self.x / other.x
+        y = self.y / other.y
+        z = self.z / other.z
+        return Point(x, y, z)
+
+
+point = Point(1, 2, 3)
+print(point)
+point1 = Point(1, 2, 3)
+new_point = +point
+print(new_point)
